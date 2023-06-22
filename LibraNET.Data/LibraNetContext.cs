cc5c22d5bbc050968f886dce6e223a4ccf5392ac
Fikata12 <filipkarabashev@gmail.com>
2023-06-23 01:18:28 +0300

    Separate the logic into layers

---
+++ LibraNET.Data/LibraNetContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraNET.Data
+{
+    public class LibraNetContext : DbContext
+    {
+        public LibraNetContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected LibraNetContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- LibraNET.Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ LibraNET.Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -8,7 +8,7 @@ using System;
 
 namespace LibraNET.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(LibraNetContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
