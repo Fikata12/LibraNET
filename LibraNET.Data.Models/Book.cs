285c4006206f55275cb8abf0264107eb33e453d2
Fikata12 <filipkarabashev@gmail.com>
2023-06-23 17:43:58 +0300

    Add all models and logic to some of them

---
+++ LibraNET.Data.Models/Book.cs
@@ -0,0 +1,55 @@
+﻿using LibraNET.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static LibraNET.Common.ValidationConstants.Book;
+
+namespace LibraNET.Data.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+            Id = Guid.NewGuid();
+            BooksAuthors = new List<BookAuthor>();
+            BooksCategories = new List<BookCategory>();
+            Comments = new List<Comment>();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLenght)]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(ISBNLength)]
+        public string? ISBN { get; set; }
+
+        [Required]
+        public DateTime PublicationDate { get; set; }
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public int PageCount { get; set; }
+
+        [Required]
+        public AgeGroup AgeGroup { get; set; }
+
+        [Required]
+        [MaxLength(LanguageMaxLength)]
+        public string Language { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Publisher))]
+        public int PublisherId { get; set; }
+
+
+        public ICollection<BookAuthor> BooksAuthors { get; set; }
+        public ICollection<BookCategory> BooksCategories { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public Publisher Publisher { get; set; } = null!;
+    }
+}
---
+++ LibraNET.Data.Models/Enums/AgeGroup.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraNET.Data.Models.Enums
+{
+    public enum AgeGroup
+    {
+        All = 0,
+        Children = 1,
+        Adolescents = 2,
+        Adults = 3
+    }
+}
+    
