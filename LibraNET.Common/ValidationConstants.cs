38822fd23db55c0912e8cad0f0d8e612572d4541
Fikata12 <filipkarabashev@gmail.com>
2023-06-23 17:41:48 +0300

    Add ValidationConstants class

---
+++ LibraNET.Common/ValidationConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace LibraNET.Common
+{
+    public static class ValidationConstants
+    {
+        public static class Book
+        {
+            public const int TitleMaxLenght = 120;
+            public const int TitleMinLenght = 1;
+
+            public const int ISBNLength = 13;
+
+            public const int DescriptionMaxLength = 2100;
+            public const int DescriptionMinLength = 600;
+
+            public const int PageCountMax = 4300;
+            public const int PageCountMin = 1;
+
+            public const int LanguageMaxLength = 20;
+            public const int LanguageMinLength = 3;
+        }
+    }
+}
