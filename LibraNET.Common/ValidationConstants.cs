cd450fef9aaba73a851d77bcee7fe2e01a2e8486
Fikata12 <filipkarabashev@gmail.com>
2023-06-24 01:49:14 +0300

    Add more validation constants

--- LibraNET.Common/ValidationConstants.cs
+++ LibraNET.Common/ValidationConstants.cs
@@ -10,13 +10,46 @@
             public const int ISBNLength = 13;
 
             public const int DescriptionMaxLength = 2100;
-            public const int DescriptionMinLength = 600;
+            public const int DescriptionMinLength = 30;
 
             public const int PageCountMax = 4300;
             public const int PageCountMin = 1;
 
             public const int LanguageMaxLength = 20;
             public const int LanguageMinLength = 3;
+        }
+
+        public static class Author
+        {
+            public const int FirstNameMaxLenght = 50;
+            public const int FirstNameMinLenght = 2;
+
+            public const int MiddleNameMaxLenght = 50;
+            public const int MiddleNameMinLenght = 2;
+
+            public const int LastNameMaxLenght = 50;
+            public const int LastNameMinLenght = 2;
+
+            public const int DescriptionMaxLength = 2100;
+            public const int DescriptionMinLength = 30;
+        }
+
+        public static class Category
+        {
+            public const int NameMaxLenght = 30;
+            public const int NameMinLenght = 3;
+        }
+
+        public static class Comment
+        {
+            public const int TextMaxLenght = 300;
+            public const int TextMinLenght = 1;
+        }
+
+        public static class Publisher
+        {
+            public const int NameMaxLenght = 50;
+            public const int NameMinLenght = 3;
         }
     }
 }
