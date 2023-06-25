3a28baec8cd5c778f1d977f9afb8932ea857ba26
Fikata12 <filipkarabashev@gmail.com>
2023-06-25 12:23:08 +0300

    Add DbSets

--- LibraNET.Data/LibraNetDbContext.cs
+++ LibraNET.Data/LibraNetDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LibraNET.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LibraNET.Data
@@ -9,8 +10,33 @@ namespace LibraNET.Data
         {
         }
 
+        public DbSet<Book> Books { get; set; } = null!;
+
+        public DbSet<Author> Authors { get; set; } = null!;
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        public DbSet<Comment> Comments { get; set; } = null!;
+
+        public DbSet<Publisher> Publishers { get; set; } = null!;
+
+        public DbSet<Rating> Ratings { get; set; } = null!;
+
+        public DbSet<BookAuthor> BooksAuthors { get; set; } = null!;
+
+        public DbSet<BookCategory> BooksCategories { get; set; } = null!;
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Rating>()
+                .HasKey(e => new { e.BookId, e.RaterId });
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(e => new { e.BookId, e.AuthorId });
+
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(e => new { e.BookId, e.CategoryId }); 
 
             base.OnModelCreating(modelBuilder);
         }
