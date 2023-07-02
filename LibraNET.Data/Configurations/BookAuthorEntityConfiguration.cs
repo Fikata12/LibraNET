11a60b484629036fc637d7b6326a4cf62bbae9e0
Fikata12 <filipkarabashev@gmail.com>
2023-07-03 00:58:48 +0300

    Add some book configuration

---
+++ LibraNET.Data/Configurations/BookAuthorEntityConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using LibraNET.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LibraNET.Data.Configurations
+{
+	internal class BookAuthorEntityConfiguration : IEntityTypeConfiguration<BookAuthor>
+	{
+		public void Configure(EntityTypeBuilder<BookAuthor> builder)
+		{
+			builder.HasData(new BookAuthor[]
+			{
+				new BookAuthor
+				{
+					BookId = Guid.Parse("234e7f45-82e2-4951-b993-0e0360daff8c"),
+					AuthorId = Guid.Parse("7345ddb8-f6da-4f88-aedc-783cee9540ec")
+				},
+				new BookAuthor
+				{
+					BookId = Guid.Parse("000"),
+					AuthorId = Guid.Parse("000")
+				},
+				new BookAuthor
+				{
+					BookId = Guid.Parse("000"),
+					AuthorId = Guid.Parse("000")
+				},
+				new BookAuthor
+				{
+					BookId = Guid.Parse("000"),
+					AuthorId = Guid.Parse("000")
+				},
+				new BookAuthor
+				{
+					BookId = Guid.Parse("000"),
+					AuthorId = Guid.Parse("000")
+				}
+			});
+		}
+	}
+}
---
+++ LibraNET.Data/Configurations/BookCategoryEntityConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using LibraNET.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LibraNET.Data.Configurations
+{
+	internal class BookCategoryEntityConfiguration : IEntityTypeConfiguration<BookCategory>
+	{
+		public void Configure(EntityTypeBuilder<BookCategory> builder)
+		{
+			builder.HasData(new BookCategory[]
+			{
+				new BookCategory
+				{
+					BookId = Guid.Parse("234e7f45-82e2-4951-b993-0e0360daff8c"),
+					CategoryId = Guid.Parse("45ba1bb0-d9d5-485a-a6c0-1d691ab334ed")
+				},
+				new BookCategory
+				{
+					BookId = Guid.Parse("000"),
+					CategoryId = Guid.Parse("000")
+				},
+				new BookCategory
+				{
+					BookId = Guid.Parse("000"),
+					CategoryId = Guid.Parse("000")
+				},
+				new BookCategory
+				{
+					BookId = Guid.Parse("000"),
+					CategoryId = Guid.Parse("000")
+				},
+				new BookCategory
+				{
+					BookId = Guid.Parse("000"),
+					CategoryId = Guid.Parse("000")
+				},
+			});
+		}
+	}
+}
