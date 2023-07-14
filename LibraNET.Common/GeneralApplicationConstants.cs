c3cfe34fc8f2d9b2a826aebed77662789086e891
Fikata12 <filipkarabashev@gmail.com>
2023-07-14 21:59:19 +0300

    Add BookController and GeneralApplicationConstants

---
+++ LibraNET.Common/GeneralApplicationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraNET.Common
+{
+	public class GeneralApplicationConstants
+	{
+		public const int DefaultPage = 1;
+		public const int BooksPerPage = 9;
+	}
+}
---
+++ LibraNET/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using LibraNET.Services.Data.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraNET.Controllers
+{
+	public class BookController : BaseController
+	{
+		private readonly IBookService bookService;
+
+		public BookController(IBookService bookService)
+		{
+			this.bookService = bookService;
+		}
+		public IActionResult All()
+		{
+			return View();
+		}
+	}
+}
